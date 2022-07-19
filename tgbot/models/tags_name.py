1e6a8bb76a19e5fcb3994a969af074028bc1053d
abrikk <abrormaksudov2207@gmail.com>
2022-07-19 14:05:43 +0500

    update

--- tgbot/models/tags_name.py
+++ tgbot/models/tags_name.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, ForeignKey, String, BigInteger
 
 from tgbot.services.db_base import Base
 
 
 class TagName(Base):
     __tablename__ = "tag_name"
+    id = Column(BigInteger, primary_key=True)
     type = Column(String(length=128), ForeignKey("tag_type.type", ondelete="CASCADE"))
     name = Column(String(length=128))
 
