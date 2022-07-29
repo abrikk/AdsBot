caabf3dee63db47d5fb1021c59fe7866b4ffc17c
abrikk <abrormaksudov2207@gmail.com>
2022-07-29 18:52:25 +0500

    update

--- tgbot/filters/is_group.py
+++ tgbot/filters/is_group.py
@@ -1,7 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
 
+from tgbot.config import Config
+
 
 class IsGroup(BoundFilter):
-    async def check(self, obj: types.Message | types.ChatMemberUpdated) -> bool:
-        return obj.chat.type == types.ChatType.SUPERGROUP and obj.chat.id != -1001721012737
+    async def check(self, obj: types.Message | types.ChatMemberUpdated, config: Config) -> bool:
+        return obj.chat.type in (
+            types.ChatType.GROUP,
+            types.ChatType.SUPERGROUP
+        ) and obj.chat.id != config.chats.private_group_id
