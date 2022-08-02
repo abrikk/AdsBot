92da1574df509cdd73d6b1b319e867ba1603686f
abrikk <abrormaksudov2207@gmail.com>
2022-08-02 12:11:35 +0500

    update

--- tgbot/filters/is_group.py
+++ tgbot/filters/is_group.py
@@ -11,6 +11,6 @@ class IsGroup(BoundFilter):
         config: Config = data["config"]
         await obj.bot.send_message(
             chat_id=config.chats.errors_channel_id,
-            text=f"getting update in func check"
+            text=f"getting update in func check\ntrue:?{obj.chat.type in (types.ChatType.SUPERGROUP,)}"
         )
         return obj.chat.type in (types.ChatType.SUPERGROUP,)
